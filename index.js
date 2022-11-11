c98ccced136d0c22645947b8dd9eda81f71fb910
Anji515 <anji.gd17@gmail.com>
2022-11-11 09:49:51 +0530

    fw21_0746_day-3

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ cartBtn.addEventListener("click",()=>{
 
 // Carousel using TimeOut
 
-let cDiv=document.getElementById("carosel");
+let cDiv=document.getElementById("carosel_inner");
+
+
 
 let images=[`https://images.ctfassets.net/wtodlh47qxpt/4wzmNLWjqVZZl95Fcf48r2/90bd1294b970f903545d8f0f5278b28a/Allu_Arjun_Combo_Meal__1440x396px.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/7Epy9aFbpyR8YoHuGQF3sZ/01f22a322d491c24121cd4a05a15b45f/KFC_Maggi__Banner__1440x396px_149.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/4gztBB8yAvtp6jV7JAuLD/093fddbb77a78a44a4d3d5e066c592de/KFC_Peri_Peri_Banner__1440x396px.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/500GRYvL6xfLzNRY68rr4u/c66030e22aa477594939c55281fc00fd/variety_bucket_banner_1440x396px.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/2cKs5e17FbKIE7Dza5ZlNM/e7163ee02d91d59d81a20ecf606f707b/Biryani_Banner_1440x396px.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/4qo6xWTWQmjg8ycSRETMU5/649a454a732e77c4cc534524e48bd800/Box_Meals_App_Banner__1440x396px.jpg?w=1536&fit=fill&fm=webp`,`https://images.ctfassets.net/wtodlh47qxpt/4MiZL0wr9Z4ZOTjldWEshT/5ed50dce636025cd535b9344ae820d46/DIP_N_CRUNCH_BANNER_1440x396px.jpg?w=1536&fit=fill&fm=webp`]
 
@@ -51,7 +53,7 @@ setInterval(function(){
     cDiv.append(image);
 
     i++
-  },3000);
+  },4000);
 
  
 //   Categories portion starts from here
@@ -84,7 +86,16 @@ const categories = ()=>{
 
 categories();
 
+
+// Rederecting to Products Page
 let goToCat=document.querySelector("#catMenu");
 goToCat.addEventListener("click",()=>{
     window.location.href="./product.html"
 })
+
+
+let carosel=document.querySelector("#carosel");
+carosel.addEventListener("click",()=>{
+    window.location.href="./product.html"
+})
+
